3b6e372ff407f7d7e9e8b86f7bd094b8bea46d18
Maxime479 <45390764+Maxime479@users.noreply.github.com>
2022-06-03 21:27:42 +0200

    test v1 yeelight

--- examples/remote_control/03_yeelight.py
+++ examples/remote_control/03_yeelight.py
@@ -19,34 +19,42 @@ except ModuleNotFoundError:
     print("To run this demo, you need the python package: yeelight")
     print("Can be installed with: pip install yeelight")
     import sys
+
     sys.exit()
 bulbs = discover_bulbs()
 if bulbs == []:
     print("No Yeelight bulb detected !")
-    import sys
-    sys.exit()
-bulb = Bulb(bulbs[0]['ip'])
+    # import sys
+    # sys.exit()
+# bulb = Bulb(bulbs[0]['ip'])
+ip = "192.168.1.125"
+# bulbs.append(ip)
+bulb = Bulb(ip)
+print("Bulbee : ")
+print(bulb)
 bulb_last_alert_time = 0
+
 
 # Callbacks
 def toggle_light(event):
     event.print_line()
     bulb.toggle()
 
+
 def change_preset(event):
     event.print_line()
     preset = event.name
     if preset == "PRESET 1":
-        rgb = (255,255,255)
+        rgb = (255, 255, 255)
         bulb.set_rgb(*rgb)
     elif preset == "PRESET 2":
-        rgb = (255,0,0)   
-        bulb.set_rgb(*rgb)         
+        rgb = (255, 0, 0)
+        bulb.set_rgb(*rgb)
     elif preset == "PRESET 3":
-        rgb = (0,0,255)
+        rgb = (0, 0, 255)
         bulb.set_rgb(*rgb)
     elif preset == "PRESET 4":
-        rgb = (0,255,0)
+        rgb = (0, 255, 0)
         bulb.set_rgb(*rgb)
 
 
@@ -58,7 +66,7 @@ def change_brightness(event):
     elif rotation > 0.4:
         level = "-"
     else:
-        return  
+        return
     global bulb_last_alert_time
     brightness = int(bulb.get_properties()['bright'])
     if (brightness == 1 and level == "-") or (brightness == 100 and level == "+"):
@@ -68,20 +76,21 @@ def change_brightness(event):
     else:
         bulb.set_brightness(max(0, brightness - 20))
 
+
 config = {
 
     'tracker': {'args': {'body_pre_focusing': 'higher'}},
 
-    'renderer' : {'enable': True, 'args':{'output':'toggle_light.mp4'}},
-    
-    'pose_actions' : [
+    'renderer': {'enable': True, 'args': {'output': 'toggle_light.mp4'}},
+
+    'pose_actions': [
         {'name': 'ON_OFF', 'pose': 'FIST', 'callback': 'toggle_light'},
-        {'name': 'PRESET 1', 'pose':'ONE', 'callback': 'change_preset'},
-        {'name': 'PRESET 2', 'pose':'TWO', 'callback': 'change_preset'},
-        {'name': 'PRESET 3', 'pose':'THREE', 'callback': 'change_preset'},
-        {'name': 'PRESET 4', 'pose':'FOUR', 'callback': 'change_preset'},
+        {'name': 'PRESET 1', 'pose': 'ONE', 'callback': 'change_preset'},
+        {'name': 'PRESET 2', 'pose': 'TWO', 'callback': 'change_preset'},
+        {'name': 'PRESET 3', 'pose': 'THREE', 'callback': 'change_preset'},
+        {'name': 'PRESET 4', 'pose': 'FOUR', 'callback': 'change_preset'},
         {'name': 'BRIGHTNESS', 'pose': 'FIVE', 'callback': 'change_brightness',
-        "trigger":"periodic", "first_trigger_delay":0.3, "next_trigger_delay":0.3, },
+         "trigger": "periodic", "first_trigger_delay": 0.3, "next_trigger_delay": 0.3, },
     ]
 }
 
